b1b34071feae8efbbe43e2308f12a32fa49169cb
Léandre Daumont <kekos91@free.fr>
2018-09-15 22:12:30 +0200

    Initial commit

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BlockScript : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 startPosition;
+    [SerializeField] private Vector3 endPosition;
+    [SerializeField] private Vector3[] tracksPositions; // Y will be the rotation
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position + startPosition, 1);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + endPosition, 0.9f);
+
+        foreach (Vector3 temp in tracksPositions)
+        {
+            Vector3 v = new Vector3(temp.x, 0, temp.z);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position + v, transform.position + v + Vector3.up);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(transform.position + v + Vector3.up, 0.1f);
+            Gizmos.color = Color.red;
+
+
+            Vector3 pivot = transform.position + v + Vector3.up;
+            Vector3 point = transform.position + v + Vector3.up + Vector3.forward * 0.2f;
+
+            Vector3 newDirection = Quaternion.Euler(new Vector3(0f, temp.y, 0f)) * (point - pivot) + pivot;
+
+            Gizmos.DrawLine(pivot, newDirection);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(newDirection, 0.05f);
+
+            // Vector3.up + 
+        }
+    }
+
+    public Vector3 GetEndPosition()
+    {
+        return endPosition;
+    }
+
+    public Vector3 GetStartPosition()
+    {
+        return startPosition;
+    }
+}
---
+++ Assets/Scripts/DynamicMapScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicMapScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int blocksAhead = 2;
+    [SerializeField] private GameObject[] blocksToSpawn;
+    [SerializeField] private Transform mapContainerTransform;
+    [SerializeField] private SwipeDetection player;
+
+    private List<GameObject> spawnedBlocks;
+    private float longuestBlock = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (GameObject g in blocksToSpawn)
+        {
+            BlockScript b = g.GetComponent<BlockScript>();
+            float size = b.GetEndPosition().z - b.GetStartPosition().z;
+
+            longuestBlock = Mathf.Max(longuestBlock, size);
+        }
+        spawnedBlocks = new List<GameObject>();
+        NewLevel();
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(0, 0, -speed * Time.deltaTime);
+        
+        List<GameObject> blocksToDelete = new List<GameObject>();
+        
+        foreach (GameObject g in spawnedBlocks)
+        {
+            Transform t = g.transform;
+            t.Translate(movement);
+
+            if (t.localPosition.z < -longuestBlock)
+            {
+                blocksToDelete.Add(g);
+            }
+        }
+
+        foreach (GameObject g in blocksToDelete)
+        {
+            spawnedBlocks.Remove(g);
+            Destroy(g);
+            SpawnRandomBlock();
+        }
+    }
+
+    void SpawnRandomBlock()
+    {
+        int i = Random.Range(0, blocksToSpawn.Length);
+        GameObject blockToSpawn = blocksToSpawn[i];
+        BlockScript blockToSpawnScript = blockToSpawn.GetComponent<BlockScript>();
+
+        GameObject lastBlock = spawnedBlocks[spawnedBlocks.Count - 1];
+        BlockScript lastBlockScript = lastBlock.GetComponent<BlockScript>();
+
+        GameObject spawnedBlock = Instantiate(blockToSpawn, lastBlock.transform.position + (lastBlockScript.GetEndPosition() - blockToSpawnScript.GetStartPosition()), Quaternion.identity, mapContainerTransform);
+        spawnedBlocks.Add(spawnedBlock);
+    }
+
+    public void NewLevel()
+    {
+        if (spawnedBlocks.Count > 0)
+        {
+            for (int x = spawnedBlocks.Count - 1; x >= 0; x--)
+            {
+                GameObject g = spawnedBlocks[x];
+                Destroy(g);
+                spawnedBlocks.Remove(g);
+            }
+        }
+
+        spawnedBlocks.Add(Instantiate(blocksToSpawn[0], Vector3.zero, Quaternion.identity, mapContainerTransform));
+
+        while (blocksAhead > spawnedBlocks.Count)
+        {
+            SpawnRandomBlock();
+        }
+    }
+}
